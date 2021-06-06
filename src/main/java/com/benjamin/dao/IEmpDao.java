3e005c03349b715ed9317b2d599c7f55c86f54a1
GentlyRain <3294872564@qq.com>
2021-06-06 14:08:36 +0800

    封装数据库交互的init，destory等方法；新增根据雇员编号查找；添加根据雇员名实现模糊查询

--- src/main/java/com/benjamin/dao/IEmpDao.java
+++ src/main/java/com/benjamin/dao/IEmpDao.java
@@ -11,4 +11,10 @@ public interface IEmpDao {
 
     // 查询所有员工
     List<Emp> selectAllEmps();
+
+    // 根据员工编号查找
+    List<Emp> selectEmpsByEmpNo(Integer empNo);
+
+    // 根据名称查找（模糊查询）
+    List<Emp> selectEmpsByEName(String eName);
 }
--- src/test/java/com/benjamin/test/OracleTest.java
+++ src/test/java/com/benjamin/test/OracleTest.java
@@ -8,6 +8,9 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,35 +18,121 @@ import java.util.List;
 
 public class OracleTest {
 
-    public static void main(String[] args) throws IOException {
+    private InputStream in;
+    private SqlSession session;
+    private IEmpDao empDao;
+
+    @Before
+    public void init() throws IOException {
 
         // 1.读取配置文件
-        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
 
         // 2.创建SqlSessionFactoryBuilder工厂
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
         SqlSessionFactory factory = builder.build(in);
 
         // 3.使用工厂生产SqlSession对象
-        SqlSession session = factory.openSession();
+        session = factory.openSession();
 
-        // 4.使用session创建dao的测试对象
-        IEmpDao emp = session.getMapper(IEmpDao.class);     // 查询所有员工
-        IDeptDao dept = session.getMapper(IDeptDao.class);  // 查询所有部门
+        // 4.获取dao代理对象
+        empDao = session.getMapper(IEmpDao.class);
+    }
 
-        // 5.使用session对象执行方法
-        List<Emp> empList = emp.selectAllEmps();
-        for (Emp e : empList) {
-            System.out.println(e);
-        }
-        System.out.println();
-        List<Dept> deptList = dept.selectAllDepts();
-        for (Dept d : deptList) {
-            System.out.println(d);
-        }
+    @After
+    public void destory() throws IOException {
+
+        // 提交事务
+        session.commit();
 
         // 6.关闭
         session.close();
         in.close();
+    }
+
+
+
+//    public static void main(String[] args) throws IOException {
+//
+//        // 1.读取配置文件
+//        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
+//
+//        // 2.创建SqlSessionFactoryBuilder工厂
+//        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+//        SqlSessionFactory factory = builder.build(in);
+//
+//        // 3.使用工厂生产SqlSession对象
+//        SqlSession session = factory.openSession();
+//
+//        // 4.使用session创建dao的测试对象
+//        IEmpDao emp = session.getMapper(IEmpDao.class);     // 查询所有员工
+//        IDeptDao dept = session.getMapper(IDeptDao.class);  // 查询所有部门
+//
+//        // 5.使用session对象执行方法
+//        List<Emp> empList = emp.selectAllEmps();
+//        for (Emp e : empList) {
+//            System.out.println(e);
+//        }
+//        System.out.println();
+//        List<Dept> deptList = dept.selectAllDepts();
+//        for (Dept d : deptList) {
+//            System.out.println(d);
+//        }
+//
+//        // 6.关闭
+//        session.close();
+//        in.close();
+//    }
+
+
+
+    /**
+     * 测试查询所有雇员
+     */
+    @Test
+    public void testSelectAllEmps() {
+
+        // 5.使用session对象执行方法
+        List<Emp> empList = empDao.selectAllEmps();
+
+        for (Emp emp : empList) {
+            System.out.println(emp);
+        }
+    }
+
+
+
+    /**
+     * 测试根据员工编号查找
+     */
+    @Test
+    public void testSelectEmpsByEmpNo() {
+
+        Integer empNo = 7654;
+
+        // 5.使用session对象执行方法
+        List<Emp> empList = empDao.selectEmpsByEmpNo(empNo);
+
+        for (Emp emp : empList) {
+            System.out.println(emp);
+        }
+    }
+
+
+
+    /**
+     * 测试根据名称查找（模糊查询）
+     */
+    @Test
+    public void testSelectEmpsByEName() {
+
+        String eName = "S";
+
+        // 5.使用session对象执行方法
+        List<Emp> empList = empDao.selectEmpsByEName("%" + eName + "%");
+
+        for (Emp emp : empList) {
+            System.out.println(emp);
+        }
     }
 }
